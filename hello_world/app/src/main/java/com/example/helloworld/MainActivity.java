a38012ac16107f817246d1fade18ada0e5104ae8
shriraj29 <shrirajktg@gmail.com>
2022-10-19 16:45:00 +0200

    new MAJ

--- hello_world/app/src/main/java/com/example/helloworld/MainActivity.java
+++ hello_world/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+/**
+ * Example local unit test, which will execute on the development machine (host).
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
